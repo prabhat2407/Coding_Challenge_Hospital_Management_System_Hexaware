14aa9c7882b89ab3a0ce41a8c0bd35666d2cf430
Prabhat <prabhatgprai@gmail.com>
2023-12-13 17:39:33 +0530

    Coding Challenge Hexaware

---
+++ dao/HospitalService.py
@@ -0,0 +1,18 @@
+class HospitalService:
+    def get_appointment_by_id(self, appointment_id):
+        pass
+
+    def get_appointments_for_patient(self, patient_id):
+        pass
+
+    def get_appointments_for_doctor(self, doctor_id):
+        pass
+
+    def schedule_appointment(self, appointment):
+        pass
+
+    def update_appointment(self, date, new_description, appointment_id):
+        pass
+
+    def cancel_appointment(self, appointment_id):
+        pass
